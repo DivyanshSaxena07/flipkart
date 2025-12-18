3c44de9f49b523af1647d38ef26e55466ed85a77
DivyanshSaxena07 <divyanshsaxena7700@gmail.com>
2025-12-18 20:46:11 +0530

    Remove unused navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar/navbar';
+// import Navbar from './components/navbar/navbar';
 import Home from './components/Home/Home';
 import ProductPage from './components/ProductPage/ProductPage';
 import AddAddressPage from './components/AddAddressPage/AddAddressPage';
